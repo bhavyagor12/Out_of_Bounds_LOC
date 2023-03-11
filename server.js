c347678a7d061981f22570b3e0e31f52496f3f70
Bhavya Gor <bhavya.gor9999@gmail.com>
2023-03-11 11:34:25 +0530

    init backend

---
+++ server.js
@@ -0,0 +1,43 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import morgan from "morgan";
+
+dotenv.config();
+
+morgan("tiny");
+
+const connect = () => {
+  mongoose
+    .connect(
+      `mongodb+srv://bhavya_gor:${process.env.USERPASSWORD}@cluster0.kjdrvny.mongodb.net/?retryWrites=true&w=majority`
+    )
+    .then(() => {
+      console.log("connected to db");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan("tiny"));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+app.listen(8000, () => {
+  connect();
+  console.log("Server on");
+});
