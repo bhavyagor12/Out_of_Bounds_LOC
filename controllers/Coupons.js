f31725e8878a844a1b0a8be26261c17bc7e58842
Bhavya Gor <bhavya.gor9999@gmail.com>
2023-03-12 03:28:18 +0530

    added rule engine

--- controllers/Coupons.js
+++ controllers/Coupons.js
@@ -6,38 +6,83 @@ import {
   push,
   update,
 } from "firebase/database";
+import { Engine } from "json-rules-engine";
+
+const getCouponData = async (couponID) => {
+  const db = getDatabase();
+  const starCountRef = ref(db, `user/${couponID}`);
+  return new Promise((resolve, reject) => {
+    onValue(
+      starCountRef,
+      (snapshot) => {
+        const couponData = snapshot.val();
+        resolve(couponData);
+      },
+      (error) => {
+        reject(error);
+      }
+    );
+  });
+};
 
 export const checkoutFunction = async (req, res, next) => {
   const userID = req.body.userId || "";
   const couponID = req.body.couponId || "";
-  const db = getDatabase();
-  const starCountRef = ref(db, `user/${couponID}`);
-
-  let couponData;
-  onValue(starCountRef, (snapshot) => {
-    couponData = snapshot.val();
+  try {
+    const couponData = await getCouponData(couponID);
     const userIndex = couponData.users.indexOf(userID);
     if (userIndex > -1) {
-      // fixed typo here
       couponData.users.splice(userIndex, 1);
+      const updates = {};
+      updates["user/" + couponID] = couponData;
+      console.log(update(ref(db), updates));
+      res.send("completed");
+    } else {
+      res.send("error");
     }
-    const updates = {};
-    updates["user/" + couponID] = couponData;
-    console.log(update(ref(db), updates));
-    res.send("completed");
-  });
-  res.err("error");
+  } catch (e) {
+    res.send("error");
+  }
 };
 
 export const validateCoupon = async (req, res, next) => {
-  const ref = db.ref("user/data");
-  ref.on(
-    "value",
-    (snapshot) => {
-      const getData = console.log(snapshot.val());
+  const engine = new Engine();
+  const rule = {
+    conditions: {
+      all: [
+        {
+          fact: "age",
+          operator: "greaterThanInclusive",
+          value: 30,
+        },
+        {
+          fact: "country",
+          operator: "equal",
+          value: "US",
+        },
+      ],
     },
-    (errorObject) => {
-      console.log("The read failed: " + errorObject.name);
-    }
-  );
+    event: {
+      type: "adult-content-blocked",
+    },
+  };
+  engine.addRule(rule);
+  const facts = {
+    age: 40,
+    country: "IN",
+  };
+  engine
+    .run(facts)
+    .then((events) => {
+      const results = events.results;
+      results.map((r) => {
+        console.log(r.result);
+      });
+      const failureResults = events.failureResults;
+      failureResults.map((r) => {
+        console.log(r.result);
+      });
+    })
+
+    .catch((err) => console.log(err));
 };
--- routes/Coupon.js
+++ routes/Coupon.js
@@ -1,7 +1,7 @@
 import express from "express";
-import { checkoutFunction } from "../controllers/Coupons.js";
+import { checkoutFunction, validateCoupon } from "../controllers/Coupons.js";
 
 const router = express.Router();
 router.post("/checkout", checkoutFunction);
-
+router.post("/validate", validateCoupon);
 export default router;
